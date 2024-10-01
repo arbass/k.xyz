337c1c28626e44f01ceecaf5eeb858494aabbc29
arbass <13ar.bass@gmail.com>
2024-10-01 11:09:58 +0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
-import { func_draggableElem } from '$utils/draggable-elements';
+// import { func_draggableElem } from '$utils/draggable-elements';
+// import { func_interactiveZone } from '$utils/interactive-zone';
 import { func_mindConnections } from '$utils/mind-connections';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
   func_mindConnections();
-  func_draggableElem();
+  // func_draggableElem();
+  // func_interactiveZone();
 });
