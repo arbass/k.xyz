4138a2f89aa11b1d792bf5610f411f87e5713094
arbass <13ar.bass@gmail.com>
2024-10-09 18:54:37 +0300

    updated:hero form

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 // import { func_draggableElem } from '$utils/draggable-elements';
 // import { func_interactiveZone } from '$utils/interactive-zone';
+import { func_heroForm } from '$utils/hero-form';
 import { func_mindConnections } from '$utils/mind-connections';
 
 window.Webflow ||= [];
@@ -7,4 +8,5 @@ window.Webflow.push(() => {
   func_mindConnections();
   // func_draggableElem();
   // func_interactiveZone();
+  func_heroForm();
 });
