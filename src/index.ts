b730b17e9674b00a0aca9353acfbfa66575eb4d8
arbass <13ar.bass@gmail.com>
2024-10-14 14:53:58 +0300

    updated

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 // import { func_draggableElem } from '$utils/draggable-elements';
 // import { func_interactiveZone } from '$utils/interactive-zone';
+import { func_buttonTextToggl } from '$utils/button-text-toggl';
+import { func_collapseButtons } from '$utils/collapse-buttons';
+import { func_heightTransition } from '$utils/height-transition';
 import { func_heroForm } from '$utils/hero-form';
 import { func_mindConnections } from '$utils/mind-connections';
+import { func_syncClick } from '$utils/sync-click';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
@@ -9,4 +13,8 @@ window.Webflow.push(() => {
   // func_draggableElem();
   // func_interactiveZone();
   func_heroForm();
+  func_collapseButtons();
+  func_heightTransition();
+  func_syncClick();
+  func_buttonTextToggl();
 });
